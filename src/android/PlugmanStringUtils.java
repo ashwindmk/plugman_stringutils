08bf9e81cb81cb7a4b0b30667da406d4478decba
Ashwin <ashwin.dinesh01@gmail.com>
2018-07-09 13:10:38 +0530

    Changed callback result to json object

--- src/android/PlugmanStringUtils.java
+++ src/android/PlugmanStringUtils.java
@@ -8,17 +8,18 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import ashwin.libraries.com.androidstringutils.StringUtils;
+import jdk.nashorn.api.scripting.JSObject;
 
 public class PlugmanStringUtils extends CordovaPlugin {
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
         if (action.equals("isBlank")) {
-            String input = args.getString(0);
+            String input = args.getJSONObject(0).getString("input");
             this.isBlank(input, callbackContext);
             return true;
         }
         if (action.equals("isEmpty")) {
-            String input = args.getString(0);
+            String input = args.getJSONObject(0).getString("input");
             this.isEmpty(input, callbackContext);
             return true;
         }
@@ -27,7 +28,9 @@ public class PlugmanStringUtils extends CordovaPlugin {
 
     private void isBlank(String input, CallbackContext callbackContext) {
         try {
-            callbackContext.success(StringUtils.isBlank(input));
+            JSONObject result = new JSONObject();
+            result.put("result", StringUtils.isBlank(input));
+            callbackContext.success(result);
         } catch (Exception e) {
             callbackContext.error("Exception occured: " + e);
         }
@@ -35,7 +38,9 @@ public class PlugmanStringUtils extends CordovaPlugin {
 
     private void isEmpty(String input, CallbackContext callbackContext) {
         try {
-            callbackContext.success(StringUtils.isEmpty(input));
+            JSONbject result = new JSONObject();
+            result.put("result", StringUtils.isEmpty(input));
+            callbackContext.success(result);
         } catch (Exception e) {
             callbackContext.error("Exception occured: " + e);
         }
